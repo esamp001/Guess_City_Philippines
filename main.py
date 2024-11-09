3caaa78b40d9584746fa05ad58407caf60de8ea7
esamp001 <ellejunsampaga@gmail.com>
2024-11-09 21:14:52 +0800

    new

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import turtle
+import pandas as pd
+
+Philippine_data = pd.read_csv("province_coordinate1.csv", encoding='latin1')
 
 screen = turtle.Screen()
-screen.title("Guess Philippine City")
-image = "blank_states_img.gif"
-
+screen.setup(800, 800)
+screen.title("Guess Philippine Provinces")
+image = "PH_MAP.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-screen.exitonclick()
+answer_province = screen.textinput(title="Guess the Province", prompt="How many provinces can you guess on the Philippine map?")
+
+
+if answer_province in Philippine_data["Provinces"].values:
+    # Get the corresponding 'Coordinates' for the matching province
+    coordinates = Philippine_data.loc[Philippine_data["Provinces"] == answer_province, "Coordinates"].values[0]
+    print(coordinates)
