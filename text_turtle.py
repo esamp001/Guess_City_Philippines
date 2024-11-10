cd9a0476eb661ba6bdd7e1c8194d0a285873ddd3
sampaga <ellejunsampaga@gmail.com>
2024-11-10 19:46:57 +0800

    new changes

--- text_turtle.py
+++ text_turtle.py
@@ -1,10 +1,9 @@
-from turtle import Turtle
+from turtle import Turtle,Screen
 import pandas as pd
-from turtle import Screen
 
 Philippine_data = pd.read_csv("province_coordinate1.csv", encoding='latin1')
-x = ""
-y = ""
+
+
 class GuessedText(Turtle):
     def __init__(self):
         super().__init__()
@@ -12,15 +11,19 @@ class GuessedText(Turtle):
         self.color("red")
         self.shapesize(.5)
 
+
     def get_coordinates_in_csv(self):
         screen = Screen()
         answer_province = screen.textinput(title="Guess the Province",prompt="How many provinces can you guess on the Philippine map?")
-        if answer_province in Philippine_data["Provinces"].values:
-            # Get the corresponding 'Coordinates' for the matching province
-            coordinates = Philippine_data.loc[Philippine_data["Provinces"] == answer_province, "Coordinates"].values[0]
-            for (x, y) in coordinates:
-                x.append(x)
-                y.append(y)
+        coordinates = Philippine_data.loc[Philippine_data['Provinces'] == answer_province, 'Coordinates']
+        coordinates_x_y = coordinates.tolist()
+        print(len(coordinates_x_y))
+
+
+
+
+
+
 
 
 
